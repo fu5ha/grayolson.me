c2b0b7b8347455e814aa51dee41f77826eb4f41c
Gray Olson <gray@grayolson.com>
2017-01-07 12:34:00 -0700

    fix styles

--- scripts/phenomic.browser.js
+++ scripts/phenomic.browser.js
@@ -7,11 +7,11 @@ import phenomicClient from "phenomic/lib/client"
 import metadata from "../src/metadata.js"
 import routes from "../src/routes.js"
 import store from "../src/store.js"
-import { StyleSheet } from  "aphrodite"
+// import { StyleSheet } from  "aphrodite"
 
-if (window._aphrodite) {
-  StyleSheet.rehydrate(window._aphrodite)
-}
+// if (window._aphrodite) {
+//   StyleSheet.rehydrate(window._aphrodite)
+// }
 
 phenomicClient({ metadata, routes, store })
 
